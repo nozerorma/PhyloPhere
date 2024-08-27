f3f9d5a872d8d284b3453e14df9a8a0d69b2f265
nozerorma <miralnso@proton.me>
2024-08-27 13:01:07 +0200

    better examples + conf

--- example-sbatch_rerconverge.sh
+++ example-sbatch_rerconverge.sh
@@ -23,78 +23,64 @@
 # Ensure the Slurm log directory exists
 mkdir -p Slurm
 
-# Function to submit the array job
-submit_job() {
-    sbatch --parsable <<'EOF'
-#!/bin/bash
-#SBATCH --job-name=phylophere-rerc-mal
-#SBATCH -p haswell
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
-#SBATCH -e slurm-%j.err
-#SBATCH -o slurm-%j.out
-#SBATCH --time=24:00:00
-
-# send mail if needed
-#SBATCH --mail-type=START,END,FAIL
-#SBATCH --mail-user=mail@institution.edu
-
-# Define the directory where trait files are located
+#SBATCH --job-name=name                                             # job name
+#SBATCH -p haswell                                                  # partition (In Marvin: normal, haswell)
+#SBATCH --nodes=1                                                   # If run in local mode, set to 1. Else, to whatever needed
+#SBATCH --ntasks=1                                                  # If run in local mode, set to 1. Else, to whatever needed
+#SBATCH --cpus-per-task=8                                           # If run in local mode, set to whatever needed. Else, to 1
+#SBATCH --mem=16G                                                   # Resource hungry, 16GB should be fine
+#SBATCH -t 24:00:00                                                 # time (D-HH:MM)
+#SBATCH --exclude=mr-00-[01-25],mr-01-[01-02]                       # exclude nodes (opt)
+#SBATCH -e Slurm/slurm-%A_%a.err                                    # error file
+#SBATCH -o Slurm/slurm-%A_%a.out                                    # output file
+#SBATCH --mail-type=START,END,FAIL                                  # notifications for job to be sent
+#SBATCH --mail-user=user@provider.edu                               # email to send notifications to
 
 DATE_WITH_TIME=$(date "+%Y%m%d-%H%M%S") # add %3N as we want milliseconds too
-TRAIT_DIR="/path/goes/here/scratch/user/traitdir"
+
+# RERConverge specifics
+MY_TRAITS="/path/goes/here/user/trait_data.csv"
 RESULTS_DIR="/path/goes/here/user/outdir/RER/${DATE_WITH_TIME}"
 TRAIT_COLUMN="trait_column_name"
 SPECIES_COLUMN="species_column_name"
 
-# Define workdir
-WORK_DIR="/path/goes/here/user/PhyloPhere-work"
+## Nextflow specifics
+WORK_DIR="/path/goes/here/user/PhyloPhere-work"                     # Working directory (tmp)
 mkdir -p $WORK_DIR
 
-# DIRS
-MY_TRAITS="/path/goes/here/user/trait_data.csv"
+## If running in Marvin Normal partition                            # Load the modules needed for run in normal partition (older arch)
+#module load SciPy-bundle/2020.03-foss-2020a-Python-3.8.2
+#module load DendroPy/4.4.0-foss-2018b-Python-3.6.6
+#module load Biopython/1.75-foss-2018b-Python-3.6.6
+#module load R/3.6.0-foss-2018b
+#module load Java/11.0.2
+#module load Singularity/3.7.1-foss-2016b
+#module load Nextflow/22.10.5
 
-# Load nextflow
+## Else if running in Marvin Haswell partition                      # Load the modules needed for run in haswell partition (newer arch)
 module load Nextflow
 
-# Run rerconverge instance of Phylophere
-nextflow run main.nf -with-singularity -with-tower -profile singularity \
+# Pipeline execution
+
+### If using Singularity, add the -with-singularity flag and the -profile singularity flag
+### If using Nextflow Tower, add the -with-tower flag
+### If running in Marvin normal partition, add the -profile normal flag
+### If running in Marvin haswell partition, add the -profile haswell flag
+##### IF WE WANT TO USE SLURM AS EXECUTOR, ADD THE -profile normal_slurm OR -profile haswell_slurm FLAG AND MODIFY SBATCH PARAMETERS ACCORDINGLY
+
+nextflow run main.nf -with-singularity -with-tower -profile singularity,haswell \
     -w $WORK_DIR \
     --outdir $RESULTS_DIR \
-    --rer_tool build_trait,build_trees,build_matrix,continuous \
+    --rer_tool "build_trait,build_trees,build_matrix,continuous" \
     --my_traits $MY_TRAITS \
     --traitname $TRAIT_NAME \
     --sp_name $SPECIES_COLUMN \
     --rer_minsp 10 \
     --winsorizeRER 3 \
     --winsorizeTrait 3
-EOF
-}
 
-# Function to submit the cleanup job
-submit_cleanup_job() {
-    local job_id=$1
-    sbatch --dependency=afterok:$job_id <<'EOF'
-#!/bin/bash
-#SBATCH --job-name=cleanup
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH -p haswell
-#SBATCH --mem=2G
-#SBATCH -e Slurm/cleanup-%j.err
-#SBATCH -o Slurm/cleanup-%j.out
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=mail@institution.edu
+# If the tool has already been run, other parameters can be used to rerun the tool
+## ie. --trait_out, --trees_out
 
-rm -rf ${WORK_DIR}/*
-
-EOF
-}
-
-# Main script logic
-job_id=$(submit_job)
-submit_cleanup_job $job_id
-
+nextflow clean -f
 
