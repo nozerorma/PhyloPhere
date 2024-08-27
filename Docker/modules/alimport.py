87fd4b51d353fa878dd115c67ba5ea7fe78e1a2a
nozerorma <miralnso@proton.me>
2024-08-27 11:58:04 +0200

    add ali skip logic

--- Docker/modules/alimport.py
+++ Docker/modules/alimport.py
@@ -100,29 +100,36 @@ def filter_position(imported_position, changes_threshold, max_gaps_ratio):
 # FUNCTION slice()
 # Generates a key file per each gene
  
-def slice(alignment_file, alignment_format, column_threshold, max_gaps = 0.5):
-
+def slice(alignment_file, alignment_format, column_threshold, max_gaps=0.5):
     class slice_object():
         def __init__(self):
             self.d = []
             self.genename = ""
             self.species = []
-    
+
     z = slice_object()
-    imported_alignment = AlignIO.read(alignment_file, alignment_format)
     z.genename = alignment_file.split("/")[-1].split(".")[0]
 
-    # SPECIES IN THE ALIGNMENT
+    try:
+        # Read the alignment (this will fail if sequences are not the same length)
+        imported_alignment = AlignIO.read(alignment_file, alignment_format)
+    except ValueError as e:
+        # Handle the case where sequences are not of the same length
+        print(f"Skipping {alignment_file}: {str(e)}")
+        return z  # Return an empty slice_object instead of None
 
+    # SPECIES IN THE ALIGNMENT
     for x in imported_alignment:
         z.species.append(x.id)
-    
+
     z.species = list(set(z.species))
 
     # IMPORTING POSITIONS
-
-    imported_positions = list(map(functools.partial(import_position, imported_alignment = imported_alignment), [position for position in range(0,imported_alignment.get_alignment_length())]))
+    imported_positions = list(map(functools.partial(import_position, imported_alignment=imported_alignment),
+                                  range(imported_alignment.get_alignment_length())))
 
     # FILTERING POSITIONS
-    z.d = list(filter(functools.partial(filter_position, changes_threshold = column_threshold, max_gaps_ratio = max_gaps), imported_positions))
+    z.d = list(filter(functools.partial(filter_position, changes_threshold=column_threshold,
+                                        max_gaps_ratio=max_gaps), imported_positions))
+
     return z
