0aa0f68f29cc8227171810f2aea6450c556026c2
nozerorma <miralnso@proton.me>
2026-01-28 14:34:32 +0100

    gitignore work

--- work/de/dc0494f5bfc475e69d962383fa8816/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -euo pipefail
-cp -R /home/miguel/IBE-UPF/PhD/PhyloPhere/subworkflows/TRAIT_ANALYSIS/local/* .
-mkdir -p data_exploration/HTML_reports
-Rscript -e "rmarkdown::render('2.Phenotype_exploration.Rmd', output_dir='data_exploration/HTML_reports', quiet=TRUE)" --args           'cancer_traits_processed-LQ.csv'           'science.abn7829_data_s4.nex.tree'           'data_exploration'           '1998'           'primates'           'family'           'neoplasia_prevalence'           'adult_necropsy_count'           'neoplasia_necropsy'           '/home/miguel/IBE-UPF/PhD/PhyloPhere/Data/5.Phylogeny/taxid_species_family.tsv'           'malignant_prevalence'           'LQ'
